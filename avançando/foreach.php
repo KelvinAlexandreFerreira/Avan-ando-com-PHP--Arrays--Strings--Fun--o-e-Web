d42d7a71b5b6dfe669175a05383f9914e45a0965
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2021-09-11 17:25:45 -0400

    Adicionando dados

--- avançando/foreach.php
+++ avançando/foreach.php
@@ -1,20 +1,27 @@
 <?php
 
 $contasCorrentes = [
-    12345678910 => [
+    '123.456.789-10' => [
         'titular' => 'Vinicius',
         'saldo' => 1000
     ], 
-    12345778911 => [
+    '123.457.789-11' => [
         'titular' => 'Maria',
         'saldo' => 10000
     ], 
-    12445678988 => [
+    '124.456.789-88' => [
         'titular' => 'Alberto',
         'saldo' => 300
     ]
 ];
 
+$contasCorrentes['123.754.789-44'] = [ 
+    'titular' => 'Vinicius',
+    'saldo' => 1000
+    
+];
+
+
 foreach ($contasCorrentes as $cpf => $conta) {
-    echo $cpf . PHP_EOL;
+    echo $cpf . " " . $conta['titular'] . PHP_EOL;
 }
