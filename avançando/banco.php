8b6a2de534c16882638dd609a49fb85d7e53460d
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2021-09-12 07:09:16 -0400

    Funções

--- avançando/banco.php
+++ avançando/banco.php
@@ -1,5 +1,15 @@
 <?php
 
+function sacar($conta, $valorASacar)
+{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não pode sacar");       
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+    return $conta;
+}
+
 function exibeMensagem($mensagem)
 {
     echo $mensagem . PHP_EOL;
@@ -20,17 +30,9 @@ $contasCorrentes = [
     ]
 ];
 
-if (1500 > $contasCorrentes['123.456.789-10']['saldo']) {
-    exibeMensagem("Você não pode sacar este valor");
-} else {
-    $contasCorrentes['123.456.789-10']['saldo'] -= 1500;
-}
+$contasCorrentes['123.456.789-10'] = sacar($contasCorrentes['123.456.789-10'], 1500);
+$contasCorrentes['123.457.789-11'] = sacar($contasCorrentes['123.457.789-11'], 500);
 
-if (500 > $contasCorrentes['123.457.789-11']['saldo']) {
-    exibeMensagem("Você não pode sacar este valor");
-} else {
-    $contasCorrentes['123.457.789-11']['saldo'] -= 500;
-}
 
 foreach ($contasCorrentes as $cpf => $conta) {
     exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
